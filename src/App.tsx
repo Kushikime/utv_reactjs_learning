f797acc9a192f8f6a7fcfbbf780ab03156646750
Kushikime <demcsakerik@gmail.com>
2023-12-11 00:50:53 +0100

    add: add react-router-dom, added few test pages, configured page chunk load

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Counter } from './components/Counter';
 import './index.scss';
+import { Route, Routes, Link } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Suspense } from 'react';
 
 export const App = () => {
   return (
     <div className="app">
-      <h1>TEST</h1>
-      <Counter />
+      <Link to="/">Main</Link>
+      <Link to="/about">About me</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,15 @@
+import { Counter } from '../../components/Counter';
+
+const MainPage = () => {
+  return (
+    <div className="test">
+      MAIN PAGE
+      <div>
+        <h1>TEST</h1>
+        <Counter />
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
